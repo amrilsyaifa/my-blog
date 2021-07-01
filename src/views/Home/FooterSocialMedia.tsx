943e5ee1f62f2f7af1587f442510e5b85e4de095
amrilsyaifa <amrilsyaifa@gmail.com>
2021-07-01 08:24:07 +0700

    update

---
+++ src/views/Home/FooterSocialMedia.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import styled from 'styled-components'
+import { GitHub, Instagram, Linkedin } from 'react-feather'
+import useTheme from 'hooks/useTheme'
+import { TextProps, PropsRow } from './types'
+
+const Text = styled.div<TextProps>`
+  color: ${(props) => props.theme.color?.primary};
+  font-size: ${(props) => (props.fontSize ? props.fontSize : '16px')};
+  font-weight: ${(props) => (props.fontWeight ? props.fontWeight : 'normal')};
+  line-height: ${(props) => (props.lineHeight ? props.lineHeight : '')};
+  padding-top: ${(props) => (props.paddingTop ? props.paddingTop : '')};
+  padding-left: ${(props) => (props.paddingLeft ? props.paddingLeft : '')};
+`
+
+const ContainerSocialMedia = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  width: 90%;
+  margin-left: 10%;
+  padding-bottom: 50px;
+  @media (max-width: 600px) {
+    margin-left: 0;
+    width: 100%;
+  }
+`
+
+const Row = styled.div<PropsRow>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: ${(props) => (props.marginLeft ? props.marginLeft : '0px')};
+`
+
+const WrapIcon = styled.div`
+  cursor: pointer;
+`
+
+const FooterSocialMedia = () => {
+  const { theme } = useTheme()
+
+  const openInNewTab = (url: string) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
+  return (
+    <ContainerSocialMedia>
+      <Row>
+        <WrapIcon onClick={() => openInNewTab('https://github.com/amrilsyaifa')}>
+          <GitHub color={theme.color.primary} size={20} />
+        </WrapIcon>
+        <Text paddingLeft="3px" fontSize="14px" fontWeight="bold">
+          @amrilsyaifa
+        </Text>
+      </Row>
+      <Row marginLeft="12px">
+        <WrapIcon onClick={() => openInNewTab('https://www.linkedin.com/in/amril-syaifa-yasin-506530141/')}>
+          <Linkedin color={theme.color.primary} size={20} />
+        </WrapIcon>
+        <Text paddingLeft="3px" fontSize="14px" fontWeight="bold">
+          Amril Syaifa Yasin
+        </Text>
+      </Row>
+      <Row marginLeft="12px">
+        <WrapIcon onClick={() => openInNewTab('https://www.instagram.com/amril.syaifa/')}>
+          <Instagram color={theme.color.primary} size={20} />
+        </WrapIcon>
+        <Text paddingLeft="3px" fontSize="14px" fontWeight="bold">
+          @amrilsyaifa
+        </Text>
+      </Row>
+    </ContainerSocialMedia>
+  )
+}
+
+export default FooterSocialMedia
--- src/views/Home/SectionHead.tsx
+++ src/views/Home/SectionHead.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import Typewriter from 'typewriter-effect'
-import { GitHub, Instagram, Linkedin } from 'react-feather'
-import useTheme from 'hooks/useTheme'
-import { TextProps, PropsRow } from './types'
+import FooterSocialMedia from './FooterSocialMedia'
+import { TextProps } from './types'
 
 const Section1 = styled.section`
   display: grid;
@@ -51,39 +50,7 @@ const Text = styled.div<TextProps>`
   padding-left: ${(props) => (props.paddingLeft ? props.paddingLeft : '')};
 `
 
-const SectionFooter = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  position: absolute;
-  bottom: 0;
-  width: 90%;
-  margin-left: 10%;
-  padding-bottom: 50px;
-  @media (max-width: 600px) {
-    margin-left: 0;
-    width: 100%;
-  }
-`
-
-const Row = styled.div<PropsRow>`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-left: ${(props) => (props.marginLeft ? props.marginLeft : '0px')};
-`
-
-const WrapIcon = styled.div`
-  cursor: pointer;
-`
-
 const SectionHead = () => {
-  const { theme } = useTheme()
-
-  const openInNewTab = (url: string) => {
-    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
-    if (newWindow) newWindow.opener = null
-  }
   return (
     <Section1>
       <Wrapper>
@@ -106,32 +73,7 @@ const SectionHead = () => {
           />
         </Text>
       </Wrapper>
-      <SectionFooter>
-        <Row>
-          <WrapIcon onClick={() => openInNewTab('https://github.com/amrilsyaifa')}>
-            <GitHub color={theme.color.primary} size={20} />
-          </WrapIcon>
-          <Text paddingLeft="3px" fontSize="14px" fontWeight="bold">
-            @amrilsyaifa
-          </Text>
-        </Row>
-        <Row marginLeft="12px">
-          <WrapIcon onClick={() => openInNewTab('https://www.linkedin.com/in/amril-syaifa-yasin-506530141/')}>
-            <Linkedin color={theme.color.primary} size={20} />
-          </WrapIcon>
-          <Text paddingLeft="3px" fontSize="14px" fontWeight="bold">
-            Amril Syaifa Yasin
-          </Text>
-        </Row>
-        <Row marginLeft="12px">
-          <WrapIcon onClick={() => openInNewTab('https://www.instagram.com/amril.syaifa/')}>
-            <Instagram color={theme.color.primary} size={20} />
-          </WrapIcon>
-          <Text paddingLeft="3px" fontSize="14px" fontWeight="bold">
-            @amrilsyaifa
-          </Text>
-        </Row>
-      </SectionFooter>
+      <FooterSocialMedia />
     </Section1>
   )
 }
