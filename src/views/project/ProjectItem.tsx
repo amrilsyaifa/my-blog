6618b4da6f878caf20f2cf1204d571abb3155826
iamrils <amrilsyaifa10@gmail.com>
2024-05-22 22:21:36 +0700

    fix: font size

--- src/views/project/ProjectItem.tsx
+++ src/views/project/ProjectItem.tsx
@@ -13,11 +13,11 @@ const ProjectItem: FC<ProjectItemProps> = ({ link, title, description }) => {
   return (
     <div className="w-full md:max-w-sm p-6 border border-gray-200 rounded-lg shadow  dark:border-gray-700">
       <Link href={link} rel="noopener noreferrer" target="_blank">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight line-clamp-2 text-gray-900 dark:text-white">
+        <h5 className="mb-2 text-lg font-bold tracking-tight line-clamp-2 dark:text-[#ec7a56] text-gray-800">
           {title}
         </h5>
       </Link>
-      <p className="mb-3 font-normal line-clamp-3 text-gray-700 dark:text-gray-400">
+      <p className="mb-3 font-normal line-clamp-3 dark:text-gray-400 text-gray-800 italic">
         {description}
       </p>
     </div>
