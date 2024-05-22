33f2f0929dd56d3f351194e913860b10c0f6e6ac
iamrils <amrilsyaifa10@gmail.com>
2024-05-22 22:11:23 +0700

    fix font empty

--- src/components/EmptyData.tsx
+++ src/components/EmptyData.tsx
@@ -5,7 +5,9 @@ const EmptyData = () => {
   const t = useTranslations("common");
   return (
     <div>
-      <h1 className="text-2xl font-bold text-center">{t("empty_data")}</h1>
+      <h1 className="text-lg md:text-xl font-bold text-center">
+        {t("empty_data")}
+      </h1>
     </div>
   );
 };
