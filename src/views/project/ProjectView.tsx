fb435fea5647dc3b32a34be1b3cf649d5b115005
iamrils <amrilsyaifa10@gmail.com>
2024-05-27 22:03:07 +0700

    feat: separe project

--- src/views/project/ProjectView.tsx
+++ src/views/project/ProjectView.tsx
@@ -1,58 +1,24 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import ProjectItem, { ProjectItemProps } from "./ProjectItem";
-import { useDisclosure } from "@components/hooks/useDisclosure";
-import EmptyData from "@components/components/EmptyData";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "@components/configs/firebase";
-import ProjectShimmer from "./ProjectShimmer";
+import { useState } from "react";
+import Tab from "@components/components/Tab";
+import PersonalProject from "./PersonalProject";
+import CompanyProject from "./CompanyProject";
+import { useTranslations } from "next-intl";
 
 const ProjectView = () => {
-  const [isLoading, handle] = useDisclosure(true);
-  const [projects, setProjects] = useState<ProjectItemProps[]>([]);
-
-  const getProjects = async () => {
-    try {
-      handle.open();
-      const querySnapshot = await getDocs(collection(db, "projects"));
-      querySnapshot.forEach((doc) => {
-        setProjects((prev) => [...prev, doc.data() as ProjectItemProps]);
-      });
-      handle.close();
-    } catch (error) {
-      handle.close();
-      console.log(error);
-      alert("Failed to fetch data");
-    }
-  };
-
-  useEffect(() => {
-    getProjects();
-  }, []);
-
-  if (isLoading) {
-    return <ProjectShimmer />;
-  }
-
-  const hasEmptyData = !isLoading && projects.length === 0;
+  const t = useTranslations("project");
+  const [tab, setTab] = useState("self");
+  const tabs = [
+    { label: t("personal"), value: "self" },
+    { label: t("company"), value: "company" },
+  ];
 
   return (
     <>
-      {hasEmptyData && (
-        <div className="w-full flex items-center justify-center md:h-[calc(100vh-30em)] h-[calc(100vh-20em)]">
-          <EmptyData />
-        </div>
-      )}
-      {!hasEmptyData && (
-        <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-6 gap-4">
-          {projects
-            .sort((a, b) => b.order - a.order)
-            .map((project) => (
-              <ProjectItem {...project} key={project.id} />
-            ))}
-        </div>
-      )}
+      <Tab data={tabs} value={tab} onSelect={setTab} />
+      {tab === "self" && <PersonalProject />}
+      {tab === "company" && <CompanyProject />}
     </>
   );
 };
