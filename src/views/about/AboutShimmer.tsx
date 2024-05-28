ef56590ee29c616c4284c11e67de8b5bb248cf7c
iamrils <amrilsyaifa10@gmail.com>
2024-05-28 11:27:37 +0700

    fix: add loading

---
+++ src/views/about/AboutShimmer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AboutShimmer = () => {
+  return (
+    <div role="status" className="mt-8 w-full animate-pulse overflow-hidden">
+      <div className="h-8 bg-gray-200 rounded-md dark:bg-gray-700 w-40 mb-4" />
+      <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 w-full mb-4" />
+      <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 w-full mb-4" />
+      <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 w-full mb-4" />
+      <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 w-full mb-4" />
+      <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 w-full mb-4" />
+      <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 w-full mb-4" />
+    </div>
+  );
+};
+
+export default AboutShimmer;
