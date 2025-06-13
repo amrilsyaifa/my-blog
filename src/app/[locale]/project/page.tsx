13415b75a1c2595b1d57173d0f99261283a15fe9
iamrils <amrilsyaifa10@gmail.com>
2025-06-13 18:33:09 +0700

    fix project tab

--- src/app/[locale]/project/page.tsx
+++ src/app/[locale]/project/page.tsx
@@ -1,5 +1,4 @@
 import ProjectView from "@components/views/project/ProjectView";
-import TitleProject from "@components/views/project/Title";
 import type { Metadata } from "next";
 import { useTranslations } from "next-intl";
 
@@ -15,7 +14,6 @@ export default function Project() {
   return (
     <main className="flex min-h-[calc(100vh-5.5em)] flex-col items-start p-4 md:p-24 pt-20 bg-gray-100 dark:bg-slate-900">
       <div className="max-w-screen-xl md:max-w-screen-md mx-auto w-full">
-        <TitleProject title={t("project")} />
         <ProjectView />
       </div>
     </main>
