13e63ac49b12af51ef5d409408710caf0be401e8
iamrils <amrilsyaifa10@gmail.com>
2026-05-25 12:14:42 +0700

    fix: failed build

--- src/components/layout/Inner.tsx
+++ src/components/layout/Inner.tsx
@@ -1,17 +1,6 @@
 import React, { FC } from "react";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import { slide, opacity, perspective } from "./anim";
-
-// https://github.com/olivierlarose/nextjs-framer-page-transition/blob/main/src/components/Layout/Inner/style.scss
-
-const anim = (variants: any) => {
-  return {
-    initial: "initial",
-    animate: "enter",
-    exit: "exit",
-    variants,
-  };
-};
 
 interface InnerProps {
   children: React.ReactNode;
@@ -20,9 +9,28 @@ interface InnerProps {
 const Inner: FC<InnerProps> = ({ children }) => {
   return (
     <div className="inner">
-      <motion.div className="slide" {...anim(slide)} />
-      <motion.div className="page" {...anim(perspective)}>
-        <motion.div {...anim(opacity)}>{children}</motion.div>
+      <motion.div
+        className="slide"
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        variants={slide as Variants}
+      />
+      <motion.div
+        className="page"
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        variants={perspective as Variants}
+      >
+        <motion.div
+          initial="initial"
+          animate="enter"
+          exit="exit"
+          variants={opacity as Variants}
+        >
+          {children}
+        </motion.div>
       </motion.div>
     </div>
   );
